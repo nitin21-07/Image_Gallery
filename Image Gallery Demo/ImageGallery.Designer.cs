b023115cb2c904ffd0007b100b42e898dcb299af
nitin21-07 <nitinsaini2107@gmail.com>
2020-09-06 18:55:34 +0530

    first commit

---
+++ Image Gallery Demo/ImageGallery.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Image_Gallery_Demo
+{
+    partial class ImageGallery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageGallery));
+            C1.Win.C1Tile.PanelElement panelElement1 = new C1.Win.C1Tile.PanelElement();
+            C1.Win.C1Tile.ImageElement imageElement1 = new C1.Win.C1Tile.ImageElement();
+            C1.Win.C1Tile.TextElement textElement1 = new C1.Win.C1Tile.TextElement();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._searchBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this._search = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this._imageTileControl = new C1.Win.C1Tile.C1TileControl();
+            this.group1 = new C1.Win.C1Tile.Group();
+            this.tile1 = new C1.Win.C1Tile.Tile();
+            this.tile2 = new C1.Win.C1Tile.Tile();
+            this.tile3 = new C1.Win.C1Tile.Tile();
+            this._exportImage = new System.Windows.Forms.PictureBox();
+            this.c1PdfDocument = new C1.C1Pdf.C1PdfDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._search)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._exportImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this._imageTileControl);
+            this.splitContainer1.Panel2.Controls.Add(this._exportImage);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(762, 753);
+            this.splitContainer1.SplitterDistance = 40;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(762, 40);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this._searchBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(193, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 34);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // _searchBox
+            // 
+            this._searchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._searchBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this._searchBox.Location = new System.Drawing.Point(10, 5);
+            this._searchBox.Name = "_searchBox";
+            this._searchBox.Size = new System.Drawing.Size(244, 15);
+            this._searchBox.TabIndex = 0;
+            this._searchBox.Text = "Search Image";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this._search);
+            this.panel2.Location = new System.Drawing.Point(477, 12);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 12, 45, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(40, 16);
+            this.panel2.TabIndex = 1;
+            // 
+            // _search
+            // 
+            this._search.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._search.Image = ((System.Drawing.Image)(resources.GetObject("_search.Image")));
+            this._search.Location = new System.Drawing.Point(0, 0);
+            this._search.Margin = new System.Windows.Forms.Padding(0);
+            this._search.Name = "_search";
+            this._search.Size = new System.Drawing.Size(40, 16);
+            this._search.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this._search.TabIndex = 0;
+            this._search.TabStop = false;
+            this._search.Click += new System.EventHandler(this._search_Click);
+            this._search.Paint += new System.Windows.Forms.PaintEventHandler(this._search_Paint);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 685);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(762, 24);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.Visible = false;
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // _imageTileControl
+            // 
+            this._imageTileControl.AllowChecking = true;
+            this._imageTileControl.AllowRearranging = true;
+            this._imageTileControl.CellHeight = 78;
+            this._imageTileControl.CellSpacing = 11;
+            this._imageTileControl.CellWidth = 78;
+            // 
+            // 
+            // 
+            panelElement1.Alignment = System.Drawing.ContentAlignment.BottomLeft;
+            panelElement1.Children.Add(imageElement1);
+            panelElement1.Children.Add(textElement1);
+            panelElement1.Margin = new System.Windows.Forms.Padding(10, 6, 10, 6);
+            this._imageTileControl.DefaultTemplate.Elements.Add(panelElement1);
+            this._imageTileControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._imageTileControl.Groups.Add(this.group1);
+            this._imageTileControl.Location = new System.Drawing.Point(0, 0);
+            this._imageTileControl.Name = "_imageTileControl";
+            this._imageTileControl.Size = new System.Drawing.Size(762, 709);
+            this._imageTileControl.SurfacePadding = new System.Windows.Forms.Padding(12, 4, 12, 4);
+            this._imageTileControl.SwipeDistance = 20;
+            this._imageTileControl.SwipeRearrangeDistance = 98;
+            this._imageTileControl.TabIndex = 1;
+            this._imageTileControl.Text = "c1TileControl1";
+            this._imageTileControl.TileChecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this._imageTileControl_TileChecked);
+            this._imageTileControl.TileUnchecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this._imageTileControl_TileUnchecked);
+            this._imageTileControl.Paint += new System.Windows.Forms.PaintEventHandler(this._imageTileControl_Paint);
+            // 
+            // group1
+            // 
+            this.group1.Name = "group1";
+            this.group1.Text = "Group 1";
+            this.group1.Tiles.Add(this.tile1);
+            this.group1.Tiles.Add(this.tile2);
+            this.group1.Tiles.Add(this.tile3);
+            // 
+            // tile1
+            // 
+            this.tile1.BackColor = System.Drawing.Color.LightCoral;
+            this.tile1.Name = "tile1";
+            this.tile1.Text = "Tile 1";
+            // 
+            // tile2
+            // 
+            this.tile2.BackColor = System.Drawing.Color.Teal;
+            this.tile2.Name = "tile2";
+            this.tile2.Text = "Tile 2";
+            // 
+            // tile3
+            // 
+            this.tile3.BackColor = System.Drawing.Color.SteelBlue;
+            this.tile3.Name = "tile3";
+            this.tile3.Text = "Tile 3";
+            // 
+            // _exportImage
+            // 
+            this._exportImage.Image = ((System.Drawing.Image)(resources.GetObject("_exportImage.Image")));
+            this._exportImage.Location = new System.Drawing.Point(29, 3);
+            this._exportImage.Name = "_exportImage";
+            this._exportImage.Size = new System.Drawing.Size(135, 28);
+            this._exportImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this._exportImage.TabIndex = 0;
+            this._exportImage.TabStop = false;
+            this._exportImage.Visible = false;
+            this._exportImage.Click += new System.EventHandler(this._exportImage_Click);
+            this._exportImage.Paint += new System.Windows.Forms.PaintEventHandler(this._exportImage_Paint);
+            // 
+            // c1PdfDocument
+            // 
+            this.c1PdfDocument.DocumentInfo.Author = "";
+            this.c1PdfDocument.DocumentInfo.CreationDate = new System.DateTime(((long)(0)));
+            this.c1PdfDocument.DocumentInfo.Creator = "";
+            this.c1PdfDocument.DocumentInfo.Keywords = "";
+            this.c1PdfDocument.DocumentInfo.Producer = "ComponentOne C1Pdf";
+            this.c1PdfDocument.DocumentInfo.Subject = "";
+            this.c1PdfDocument.DocumentInfo.Title = "";
+            this.c1PdfDocument.MaxHeaderBookmarkLevel = 0;
+            this.c1PdfDocument.PdfVersion = "1.3";
+            this.c1PdfDocument.RefDC = null;
+            this.c1PdfDocument.RotateAngle = 0F;
+            this.c1PdfDocument.UseFastTextOut = true;
+            this.c1PdfDocument.UseFontShaping = true;
+            // 
+            // ImageGallery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 753);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximumSize = new System.Drawing.Size(810, 810);
+            this.MinimizeBox = false;
+            this.Name = "ImageGallery";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Image Gallery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this._search)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._exportImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox _searchBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox _search;
+        private System.Windows.Forms.PictureBox _exportImage;
+        private C1.Win.C1Tile.C1TileControl _imageTileControl;
+        private C1.Win.C1Tile.Group group1;
+        private C1.Win.C1Tile.Tile tile1;
+        private C1.Win.C1Tile.Tile tile2;
+        private C1.Win.C1Tile.Tile tile3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private C1PdfDocument c1PdfDocument;
+    }
+}
+
